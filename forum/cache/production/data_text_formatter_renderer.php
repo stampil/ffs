7e15fdb7086a19723898900817a89a8adb62841d
sgourmand <sgourmand@netinary.com>
2018-12-30 13:49:01 +0100

    theme ffs

--- forum/cache/production/data_text_formatter_renderer.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1576431406
-79
-a:1:{s:5:"class";s:53:"s9e_renderer_26c41dc017c2e5802ca6f031b6082f611b9f326f";}
--- forum/cache/production/sql_9dc91161799801b44926b824ccc5cf51.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1545993027
-SELECT ban_ip, ban_userid, ban_email, ban_exclude, ban_give_reason, ban_end FROM phpbb_banlist WHERE ban_email = '' AND (ban_userid = 1 OR ban_ip <> '')
-528
-a:3:{i:0;a:6:{s:6:"ban_ip";s:12:"36.251.43.51";s:10:"ban_userid";s:1:"0";s:9:"ban_email";s:0:"";s:11:"ban_exclude";s:1:"0";s:15:"ban_give_reason";s:4:"SPAM";s:7:"ban_end";s:1:"0";}i:1;a:6:{s:6:"ban_ip";s:12:"36.251.43.51";s:10:"ban_userid";s:1:"0";s:9:"ban_email";s:0:"";s:11:"ban_exclude";s:1:"0";s:15:"ban_give_reason";s:4:"SPAM";s:7:"ban_end";s:1:"0";}i:2;a:6:{s:6:"ban_ip";s:13:"45.64.181.181";s:10:"ban_userid";s:1:"0";s:9:"ban_email";s:0:"";s:11:"ban_exclude";s:1:"0";s:15:"ban_give_reason";s:0:"";s:7:"ban_end";s:1:"0";}}
